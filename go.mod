52f76ce31eeeb22ccd48a718a45030c23efc215e
Ville Takanen <ville.takanen@foodie.fm>
2019-07-26 10:54:55 +0300

    PUT book added, takes in basic message format

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/vitku/ase-skald-microservice
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0 // indirect
+require github.com/gin-gonic/gin v1.4.0
--- src/skald.go
+++ src/skald.go
@@ -1,16 +1,35 @@
 package main
 
-import "fmt"
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BookMetaData struct {
+	Title   string `json:"title"`
+	Creator string `json:"creator"`
+}
 
 func main() {
 	fmt.Printf("hello, world\n")
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	route := gin.Default()
+	route.PUT("/book", createBook)
+	route.Run()
+}
+
+func createBook(c *gin.Context) {
+	// var book BookMetaData
+
+	data := new(BookMetaData)
+
+	err := c.BindJSON(data)
+	if err != nil {
+		c.AbortWithError(400, err)
+		return
+	}
+
+	c.String(200, fmt.Sprintf("%#v", data))
+
 }
